834698492feb02431c25f5e61725988eb383a9f8
Huaqi Fang <578567190@qq.com>
2020-07-20 09:10:38 +0800

    doc: update doc version for 1.0.0-beta1 to 1.0.1-dev
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/doc/source/conf.py
+++ NMSIS/doc/source/conf.py
@@ -21,10 +21,10 @@ copyright = '2019-Present, Nuclei'
 author = 'Nuclei'
 
 # The short X.Y version
-version = '1.0.0'
+version = '1.0.1'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0-beta1'
+release = '1.0.1-dev'
 
 
 # -- General configuration ---------------------------------------------------
