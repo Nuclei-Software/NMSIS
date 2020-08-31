ac970093f4dc5d8c9ff0fe3f48e54a78861bdde7
Huaqi Fang <578567190@qq.com>
2020-08-31 09:21:23 +0800

    DSP: Update test cases for dsp library
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/DSP/Test/ComplexMathFunctions/part1/riscv_cmplx_math.c
+++ NMSIS/DSP/Test/ComplexMathFunctions/part1/riscv_cmplx_math.c
@@ -9,18 +9,25 @@
 #include "../common.h"
 #include "riscv_math.h"
 #include <stdint.h>
+#include "array.h"
 
 #include "../HelperFunctions/math_helper.c"
 #include "../HelperFunctions/ref_helper.c"
 
 #include <stdio.h>
 #include <stdlib.h>
-#define DELTAF32 (0.05f)
+#define DELTAF32 (2.0f)
 #define DELTAQ31 (63)
 #define DELTAQ15 (1)
 #define DELTAQ7 (1)
 
-#define ARRAY_SIZE 30
+#define ENABLE_ALL
+#define CmplxMag
+#define CmplxDotProduct
+#define CONJ
+#define MagSquared
+#define CmplxMult
+#define CmplxMultReal
 
 int test_flag_error = 0;
 
@@ -32,25 +39,11 @@ int test_flag_error = 0;
 static int DSP_MagSquared(void)
 {
     uint8_t i;
-    /* clang-format off */
-	float32_t f32_pSrc[ARRAY_SIZE*2] = {1.1f, 1.1f, 2.1f, 2.1f, 3.1f, 3.1f, 4.1f, 4.1f, 5.1f, 5.1f};
-	float32_t f32_pDst[ARRAY_SIZE];
-	float32_t f32_pDst_ref[ARRAY_SIZE];
-
-	q31_t q31_pSrc[ARRAY_SIZE*2] = {263084,268304,5336008,536308,803052,800352,107036,107036,1033420,13304320};
-	q31_t q31_pDst[ARRAY_SIZE];
-	q31_t q31_pDst_ref[ARRAY_SIZE];
-
-	q15_t q15_pSrc[ARRAY_SIZE*2] = {1000,2000,3000,4000,5000,6000,7000,8000,9000,10000,11000,12000,13000,14000,15000,16000};
-	q15_t q15_pDst[ARRAY_SIZE];
-	q15_t q15_pDst_ref[ARRAY_SIZE];
-    /* clang-format on */
-
     /**********************************************************************************/
     BENCH_START(riscv_cmplx_mag_squared_f32);
-    riscv_cmplx_mag_squared_f32(f32_pSrc, f32_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_squared_f32(f32_pSrcA, f32_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_squared_f32);
-    ref_cmplx_mag_squared_f32(f32_pSrc, f32_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_squared_f32(f32_pSrcA, f32_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (fabs(f32_pDst[i] - f32_pDst_ref[i]) > DELTAF32) {
             BENCH_ERROR(riscv_cmplx_mag_squared_f32);
@@ -62,9 +55,9 @@ static int DSP_MagSquared(void)
     BENCH_STATUS(riscv_cmplx_mag_squared_f32);
     /***Q31*******************************************************************************/
     BENCH_START(riscv_cmplx_mag_squared_q31);
-    riscv_cmplx_mag_squared_q31(q31_pSrc, q31_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_squared_q31(q31_pSrcA, q31_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_squared_q31);
-    ref_cmplx_mag_squared_q31(q31_pSrc, q31_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_squared_q31(q31_pSrcA, q31_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (labs(q31_pDst[i] - q31_pDst_ref[i]) > DELTAQ31) {
             BENCH_ERROR(riscv_cmplx_mag_squared_q31);
@@ -76,9 +69,9 @@ static int DSP_MagSquared(void)
     BENCH_STATUS(riscv_cmplx_mag_squared_q31);
     /***Q15*******************************************************************************/
     BENCH_START(riscv_cmplx_mag_squared_q15);
-    riscv_cmplx_mag_squared_q15(q15_pSrc, q15_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_squared_q15(q15_pSrcA, q15_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_squared_q15);
-    ref_cmplx_mag_squared_q15(q15_pSrc, q15_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_squared_q15(q15_pSrcA, q15_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (abs(q15_pDst[i] - q15_pDst_ref[i]) > DELTAQ15) {
             BENCH_ERROR(riscv_cmplx_mag_squared_q15);
@@ -92,31 +85,11 @@ static int DSP_MagSquared(void)
 /*
 *********************************************************************************************************
 * DSP_CmplxMult
-*
-*
-*
 *********************************************************************************************************
 */
 static int DSP_CmplxMult(void)
 {
     uint8_t i;
-    /* clang-format off */
-	float32_t f32_pSrcA[ARRAY_SIZE*2] = {1.1f, 1.2f, 2.1f, 2.2f, 3.1f, 3.2f, 4.1f, 4.2f, 5.1f, 5.2f};
-	float32_t f32_pSrcB[ARRAY_SIZE*2] = {1.2f, 1.2f, 2.2f, 2.2f, 3.2f, 3.2f, 4.2f, 4.2f, 5.2f, 5.2f};
-	float32_t f32_pDst[ARRAY_SIZE];
-	float32_t f32_pDst_ref[ARRAY_SIZE];
-
-	q31_t q31_pSrcA[ARRAY_SIZE*2] = {260084,268004,536008,536008,805002,800052,1070360,1073006,1300420,1349020};
-	q31_t q31_pSrcB[ARRAY_SIZE*2] = {260084,260804,536008,530608,800502,800052,1070036,1007036,1304200,1304920};
-	q31_t q31_pDst[ARRAY_SIZE];
-	q31_t q31_pDst_ref[ARRAY_SIZE];
-
-	q15_t q15_pSrcA[ARRAY_SIZE*2] = {520, 1300, 1045, 2400, 2530, 450, 1060, 1035, 2060, 2650};
-	q15_t q15_pSrcB[ARRAY_SIZE*2] = {250, 1010, 1510, 2100, 2105, 550, 1030, 1560, 2010, 2305};
-	q15_t q15_pDst[ARRAY_SIZE];
-	q15_t q15_pDst_ref[ARRAY_SIZE];
-    /* clang-format on */
-
     /**********************************************************************************/
     BENCH_START(riscv_cmplx_mult_cmplx_f32);
     riscv_cmplx_mult_cmplx_f32(f32_pSrcA, f32_pSrcB, f32_pDst, ARRAY_SIZE);
@@ -164,75 +137,55 @@ static int DSP_CmplxMult(void)
 /*
 *********************************************************************************************************
 * DSP_CmplxMultReal
-*
-*
-*
 *********************************************************************************************************
 */
 static int DSP_CmplxMultReal(void)
 {
     uint8_t i;
-    /* clang-format off */
-	float32_t f32_pSrcCmplx[ARRAY_SIZE*2] = {1.1f, 1.2f, 2.1f, 2.2f, 3.1f, 3.2f, 4.1f, 4.2f, 5.1f, 5.2f};
-	float32_t f32_pSrcReal[ARRAY_SIZE] = {1.2f, 1.2f, 2.2f, 2.2f, 3.2f};
-	float32_t f32_pCmplxDst[ARRAY_SIZE*2];
-	float32_t f32_pCmplxDst_ref[ARRAY_SIZE*2];
-
-	q31_t q31_pSrcCmplx[ARRAY_SIZE*2] = {1 * 2684, 1 * 2684, 2 * 2684, 2 * 2684, 3 * 2684, 3 * 2684, 4 * 2684, 4 * 2684, 5 * 2684, 5 * 2684};
-	q31_t q31_pSrcReal[ARRAY_SIZE] = {3, 5, 7, 9, 11};
-	q31_t q31_pCmplxDst[ARRAY_SIZE*2];
-	q31_t q31_pCmplxDst_ref[ARRAY_SIZE*2];
-
-	q15_t q15_pSrcCmplx[ARRAY_SIZE*2] = {-164, -166, 270, 270, 302, 331, 112, 123, 114, 215};
-	q15_t q15_pSrcReal[ARRAY_SIZE] = {38079, 15880, 16490, 25405, 11024};
-	q15_t q15_pCmplxDst[ARRAY_SIZE*2];
-	q15_t q15_pCmplxDst_ref[ARRAY_SIZE*2];
-    /* clang-format on */
-
     /**********************************************************************************/
     BENCH_START(riscv_cmplx_mult_real_f32);
-    riscv_cmplx_mult_real_f32(f32_pSrcCmplx, f32_pSrcReal, f32_pCmplxDst,
+    riscv_cmplx_mult_real_f32(f32_pSrcA, f32_pSrcB, f32_pDst,
                             ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mult_real_f32);
-    ref_cmplx_mult_real_f32(f32_pSrcCmplx, f32_pSrcReal, f32_pCmplxDst_ref,
+    ref_cmplx_mult_real_f32(f32_pSrcA, f32_pSrcB, f32_pDst_ref,
                             ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE * 2; i++) {
-        if (fabs(f32_pCmplxDst[i] - f32_pCmplxDst_ref[i]) > DELTAF32) {
+        if (fabs(f32_pDst[i] - f32_pDst_ref[i]) > DELTAF32) {
             BENCH_ERROR(riscv_cmplx_mult_real_f32);
             printf("index: %d, expect: %f, actual: %f\n", i,
-                   f32_pCmplxDst_ref[i], f32_pCmplxDst[i]);
+                   f32_pDst_ref[i], f32_pDst[i]);
             test_flag_error = 1;
         }
     }
     BENCH_STATUS(riscv_cmplx_mult_real_f32);
     /***Q31*******************************************************************************/
     BENCH_START(riscv_cmplx_mult_real_q31);
-    riscv_cmplx_mult_real_q31(q31_pSrcCmplx, q31_pSrcReal, q31_pCmplxDst,
+    riscv_cmplx_mult_real_q31(q31_pSrcA, q31_pSrcB, q31_pDst,
                             ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mult_real_q31);
-    ref_cmplx_mult_real_q31(q31_pSrcCmplx, q31_pSrcReal, q31_pCmplxDst_ref,
+    ref_cmplx_mult_real_q31(q31_pSrcA, q31_pSrcB, q31_pDst_ref,
                             ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE * 2; i++) {
-        if (labs(q31_pCmplxDst[i] - q31_pCmplxDst_ref[i]) > DELTAQ31) {
+        if (labs(q31_pDst[i] - q31_pDst_ref[i]) > DELTAQ31) {
             BENCH_ERROR(riscv_cmplx_mult_real_q31);
             printf("index: %d, expect: %x, actual: %x\n", i,
-                   q31_pCmplxDst_ref[i], q31_pCmplxDst[i]);
+                   q31_pDst_ref[i], q31_pDst[i]);
             test_flag_error = 1;
         }
     }
     BENCH_STATUS(riscv_cmplx_mult_real_q31);
     /***Q15*******************************************************************************/
     BENCH_START(riscv_cmplx_mult_real_q15);
-    riscv_cmplx_mult_real_q15(q15_pSrcCmplx, q15_pSrcReal, q15_pCmplxDst,
+    riscv_cmplx_mult_real_q15(q15_pSrcA, q15_pSrcB, q15_pDst,
                             ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mult_real_q15);
-    ref_cmplx_mult_real_q15(q15_pSrcCmplx, q15_pSrcReal, q15_pCmplxDst_ref,
+    ref_cmplx_mult_real_q15(q15_pSrcA, q15_pSrcB, q15_pDst_ref,
                             ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE * 2; i++) {
-        if (abs(q15_pCmplxDst[i] - q15_pCmplxDst_ref[i]) > DELTAQ15) {
+        if (abs(q15_pDst[i] - q15_pDst_ref[i]) > DELTAQ15) {
             BENCH_ERROR(riscv_cmplx_mult_real_q15);
             printf("index: %d, expect: %x, actual: %x\n", i,
-                   q15_pCmplxDst_ref[i], q15_pCmplxDst[i]);
+                   q15_pDst_ref[i], q15_pDst[i]);
             test_flag_error = 1;
         }
     }
@@ -242,35 +195,16 @@ static int DSP_CmplxMultReal(void)
 /*
 *********************************************************************************************************
 * DSP_CONJ
-*
-*
-*
 *********************************************************************************************************
 */
 static int DSP_CONJ(void)
 {
     uint8_t i;
-
-    /* clang-format off */
-
-	float32_t f32_pSrc[ARRAY_SIZE*2] = {1.1f, 1.1f, 2.1f, 2.1f, 3.1f, 3.1f, 4.1f, 4.1f, 5.1f, 5.1f};
-	float32_t f32_pDst[ARRAY_SIZE*2];
-	float32_t f32_pDst_ref[ARRAY_SIZE*2];
-
-	q31_t q31_pSrc[ARRAY_SIZE*2] = {1, -1, -2, 2, 3, -3, -4, 4, 5, 5,6,6,7,7,8,8};
-	q31_t q31_pDst[ARRAY_SIZE*2];
-	q31_t q31_pDst_ref[ARRAY_SIZE*2];
-
-	q15_t q15_pSrc[ARRAY_SIZE*2] = {1, 1, 2, 2, 3, 3, 4, 4, 5, 5,6,6,7,7,8,8};
-	q15_t q15_pDst[ARRAY_SIZE*2];
-	q15_t q15_pDst_ref[ARRAY_SIZE*2];
-    /* clang-format on */
-
     /**********************************************************************************/
     BENCH_START(riscv_cmplx_conj_f32);
-    riscv_cmplx_conj_f32(f32_pSrc, f32_pDst, ARRAY_SIZE);
+    riscv_cmplx_conj_f32(f32_pSrcA, f32_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_conj_f32);
-    ref_cmplx_conj_f32(f32_pSrc, f32_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_conj_f32(f32_pSrcA, f32_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (fabs(f32_pDst[i] - f32_pDst_ref[i]) > DELTAF32) {
             BENCH_ERROR(riscv_cmplx_conj_f32);
@@ -282,9 +216,9 @@ static int DSP_CONJ(void)
     BENCH_STATUS(riscv_cmplx_conj_f32);
     /***Q31*******************************************************************************/
     BENCH_START(riscv_cmplx_conj_q31);
-    riscv_cmplx_conj_q31(q31_pSrc, q31_pDst, ARRAY_SIZE);
+    riscv_cmplx_conj_q31(q31_pSrcA, q31_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_conj_q31);
-    ref_cmplx_conj_q31(q31_pSrc, q31_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_conj_q31(q31_pSrcA, q31_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (labs(q31_pDst[i] - q31_pDst_ref[i]) > DELTAQ31) {
             BENCH_ERROR(riscv_cmplx_conj_q31);
@@ -296,9 +230,9 @@ static int DSP_CONJ(void)
     BENCH_STATUS(riscv_cmplx_conj_q31);
     /***Q15*******************************************************************************/
     BENCH_START(riscv_cmplx_conj_q15);
-    riscv_cmplx_conj_q15(q15_pSrc, q15_pDst, ARRAY_SIZE);
+    riscv_cmplx_conj_q15(q15_pSrcA, q15_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_conj_q15);
-    ref_cmplx_conj_q15(q15_pSrc, q15_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_conj_q15(q15_pSrcA, q15_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (abs(q15_pDst[i] - q15_pDst_ref[i]) > DELTAQ15) {
             BENCH_ERROR(riscv_cmplx_conj_q15);
@@ -313,33 +247,18 @@ static int DSP_CONJ(void)
 /*
 *********************************************************************************************************
 * DSP_CmplxDotProduct
-*
-*
-*
 *********************************************************************************************************
 */
 static int DSP_CmplxDotProduct(void)
 {
-    /* clang-format off */
-
-	float32_t f32_pSrcA[ARRAY_SIZE*2] = {1.1f, 1.1f, 2.1f, 2.1f, 3.1f, 3.1f, 4.1f, 4.1f, 5.1f, 5.1f};
-	float32_t f32_pSrcB[ARRAY_SIZE*2] = {1.1f, 1.1f, 2.1f, 2.1f, 3.1f, 3.1f, 4.1f, 4.1f, 5.1f, 5.1f};
 	float32_t f32_realResult;
 	float32_t f32_imagResult;
 	float32_t f32_realResult_ref;
 	float32_t f32_imagResult_ref;
-
-	q31_t q31_pSrcA[ARRAY_SIZE*2] = {1 * 2684356, 1 * 2684356, 2 * 2684356, 2 * 2684356, 3 * 2684356, 3 * 2684356,
-						4 * 2684356, 4 * 2684356, 5 * 2684356, 5 * 2684356};
-	q31_t q31_pSrcB[ARRAY_SIZE*2] = {1 * 2635456, 1 * 2685456, 2 * 2684356, 2 * 2684356, 3 * 2684356, 3 * 2684356,
-						4 * 2684456, 4 * 2635456, 5 * 2684356, 5 * 2684356};
 	q63_t q31_realResult;
 	q63_t q31_imagResult;
 	q63_t q31_realResult_ref;
 	q63_t q31_imagResult_ref;
-
-	q15_t q15_pSrcA[ARRAY_SIZE*2] = {5000, 10000, 15000, 20000, 25000, 5000, 10000, 15000, 20000, 25000};
-	q15_t q15_pSrcB[ARRAY_SIZE*2] = {5000, 10000, 15000, 20000, 25000, 5000, 10000, 15000, 20000, 25000};
 	q31_t q15_realResult;
 	q31_t q15_imagResult;
 	q31_t q15_realResult_ref;
@@ -399,35 +318,16 @@ static int DSP_CmplxDotProduct(void)
 /*
 *********************************************************************************************************
 * DSP_CmplxMag
-*
-*
-*
 *********************************************************************************************************
 */
 static int DSP_CmplxMag(void)
 {
     uint8_t i;
-
-    /* clang-format off */
-
-	float32_t f32_pSrc[ARRAY_SIZE*2] = {1.1f, 1.1f, 6.3f, 8.4f, 3.1f, 3.1f, 4.1f, 4.1f, 5.1f, 5.1f};
-	float32_t f32_pDst[ARRAY_SIZE];
-	float32_t f32_pDst_ref[ARRAY_SIZE];
-
-	q31_t q31_pSrc[ARRAY_SIZE*2] = {1 * 2684356, 1 * 2684356, 2 * 2684356, 2 * 2684356, 3 * 2684356, 3 * 2684356, 4 * 2684356, 4 * 2684356, 5 * 2684356, 5 * 2684356};
-	q31_t q31_pDst[ARRAY_SIZE];
-	q31_t q31_pDst_ref[ARRAY_SIZE];
-
-	q15_t q15_pSrc[ARRAY_SIZE*2] = {5000, 10000, 15000, 20000, 25000, 5000, 10000, 15000, 20000, 25000};
-	q15_t q15_pDst[ARRAY_SIZE];
-	q15_t q15_pDst_ref[ARRAY_SIZE];
-    /* clang-format on */
-
     /**********************************************************************************/
     BENCH_START(riscv_cmplx_mag_f32);
-    riscv_cmplx_mag_f32(f32_pSrc, f32_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_f32(f32_pSrcA, f32_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_f32);
-    ref_cmplx_mag_f32(f32_pSrc, f32_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_f32(f32_pSrcA, f32_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (fabs(f32_pDst[i] - f32_pDst_ref[i]) > DELTAF32) {
             BENCH_ERROR(riscv_cmplx_mag_f32);
@@ -439,9 +339,9 @@ static int DSP_CmplxMag(void)
     BENCH_STATUS(riscv_cmplx_mag_f32);
     /***Q31*******************************************************************************/
     BENCH_START(riscv_cmplx_mag_q31);
-    riscv_cmplx_mag_q31(q31_pSrc, q31_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_q31(q31_pSrcA, q31_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_q31);
-    ref_cmplx_mag_q31(q31_pSrc, q31_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_q31(q31_pSrcA, q31_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (labs(q31_pDst[i] - q31_pDst_ref[i]) > DELTAQ31) {
             BENCH_ERROR(riscv_cmplx_mag_q31);
@@ -453,9 +353,9 @@ static int DSP_CmplxMag(void)
     BENCH_STATUS(riscv_cmplx_mag_q31);
     /***Q15*******************************************************************************/
     BENCH_START(riscv_cmplx_mag_q15);
-    riscv_cmplx_mag_q15(q15_pSrc, q15_pDst, ARRAY_SIZE);
+    riscv_cmplx_mag_q15(q15_pSrcA, q15_pDst, ARRAY_SIZE);
     BENCH_END(riscv_cmplx_mag_q15);
-    ref_cmplx_mag_q15(q15_pSrc, q15_pDst_ref, ARRAY_SIZE);
+    ref_cmplx_mag_q15(q15_pSrcA, q15_pDst_ref, ARRAY_SIZE);
     for (i = 0; i < ARRAY_SIZE; i++) {
         if (abs(q15_pDst[i] - q15_pDst_ref[i]) > DELTAQ15) {
             BENCH_ERROR(riscv_cmplx_mag_q15);
@@ -470,12 +370,24 @@ static int DSP_CmplxMag(void)
 int main()
 {
     BENCH_INIT;
+#if defined CmplxMag || defined ENABLE_ALL
     DSP_CmplxMag();
+#endif
+#if defined CmplxDotProduct || defined ENABLE_ALL
     DSP_CmplxDotProduct();
+#endif
+#if defined CONJ || defined ENABLE_ALL
     DSP_CONJ();
+#endif
+#if defined MagSquared || defined ENABLE_ALL
     DSP_MagSquared();
+#endif
+#if defined CmplxMult || defined ENABLE_ALL
     DSP_CmplxMult();
+#endif
+#if defined CmplxMultReal || defined ENABLE_ALL
     DSP_CmplxMultReal();
+#endif
     BENCH_FINISH;
     if (test_flag_error) {
         printf("test error apprears, please recheck.\n");
