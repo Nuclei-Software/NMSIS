1853969fd714d3df3d5ac1d28142a97563113fc8
Huaqi Fang <578567190@qq.com>
2022-06-17 11:21:43 +0800

    scripts: update run_nmsis.sh and download_fpga.sh
    How to run:
    ==========
    
    1. Make sure the following actions are done
    
      a) fpga_runners section in nmsis_dsp.yaml and nmsis_nn.yaml
         are adapted according to your machine, such as fpga and jtag.
      b) fpga hardware and cpu/fpga jtag debugger are connected
      c) python3 Scripts/Configs/fpga/find_usb.py will help you
         to dump usb device information connected to the pc
      d) make sure you are in /path/to/NMSIS/NMSIS
    
    2. Download neccessary fpga bits using Scripts/Configs/fpga/download_fpga.sh
    
    bash Scripts/Configs/fpga/download_fpga.sh
    
    3. Run test suites on fpga using Scripts/Configs/fpga/run_nmsis.sh
    
       CFGSET=<cfgset> FPGASET=<fpgaset> CONFIG=<config> bash Scripts/Configs/fpga/run_nmsis.sh
    
       such as:
       a) CFGSET=mini CONFIG=n300 bash Scripts/Configs/fpga/run_nmsis.sh
          This will run on mini test config, and evaluate on n300 bitstream
       b) CFGSET=full CONFIG=n300,ux900,n900 bash Scripts/Configs/fpga/run_nmsis.sh
          This will run on full test config, and evaluate on n300,ux900,n900
          bitstreams
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/Scripts/Configs/fpga/download_fpga.sh
+++ NMSIS/Scripts/Configs/fpga/download_fpga.sh
@@ -1,4 +1,5 @@
 #!/bin/env bash
+FORCE=${FORCE:-0}
 
 SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
 FPGABITS=bitstreams
@@ -15,6 +16,11 @@ function wget_file() {
     local file=$2
 
     if [ -f $file ] ; then
+        if [ "x$FORCE" == "x0" ] ; then
+            echo "$file already exist, no need to download it!"
+            return
+        fi
+        echo "$file existed, will remove it, before download it"
         rm -f $file
     fi
     echo "Download $link to $file"
@@ -35,10 +41,32 @@ function wget_and_link {
     popd
 }
 
+function wget2dir {
+    local link=$1
+    local file=$2
+    local dir=$3
+
+    pushd $SCRIPTDIR
+    mkdir -p $dir
+    wget_file $link $dir/$file
+    popd
+}
+
+function make_link_latest {
+    local bitver=${1:-202206}
+    pushd $SCRIPTDIR/$FPGABITS
+    rm -f latest
+    echo "Link $bitver to latest"
+    ln -s $bitver latest
+    popd
+}
+
 function download_bits_202206 {
-    bitprefix=$FPGABITS/202206
-    wget_and_link http://doc/cpu_signoff/_downloads/7838d023a633edfce9009ead8f27f808/n300_software_best_config_ddr200t_16M_040a67d4_a1560db8d_202203111641_v3.7.0.bit ${N300_BIT} $bitprefix
-    wget_and_link http://doc/cpu_signoff/_downloads/bcf1baf4265fa729b6d62ab445d55303/ux900_software_best_config_vpu_vcu118_100M_764e6e25f_e63ce562e_202205301028_v2.3.0.bit ${UX900_BIT} $bitprefix
+    local bitprefix=$FPGABITS/202206
+    wget2dir http://doc/cpu_signoff/_downloads/7838d023a633edfce9009ead8f27f808/n300_software_best_config_ddr200t_16M_040a67d4_a1560db8d_202203111641_v3.7.0.bit ${N300_BIT} $bitprefix
+    wget2dir http://doc/cpu_signoff/_downloads/bcf1baf4265fa729b6d62ab445d55303/ux900_software_best_config_vpu_vcu118_100M_764e6e25f_e63ce562e_202205301028_v2.3.0.bit ${UX900_BIT} $bitprefix
 }
 
 download_bits_202206
+
+make_link_latest 202206
--- NMSIS/Scripts/Configs/fpga/run_nmsis.sh
+++ NMSIS/Scripts/Configs/fpga/run_nmsis.sh
@@ -1,10 +1,14 @@
 CONFIG=${CONFIG:-n300,ux900}
 LOGROOT=${LOGROOT:-logs}
 BACKUP=${BACKUP:-Backups}
+CFGSET=${CFGSET:-mini}
+BITSET=${BITSET:-latest}
 
 SCRIPTDIR=$(dirname $(readlink -f $BASH_SOURCE))
+CFGLOC=
+FPGALOC=
 
-function help() {
+function env_help() {
     echo "You can easily setup environment by execute command below"
     echo "source ${SCRIPTDIR}/Scripts/Configs/fpga/setup.sh"
     echo "Make sure nuclei-sdk folder is lay with the same level as NMSIS project folder"
@@ -12,33 +16,65 @@ function help() {
 
 if [ "x${NUCLEI_SDK_ROOT}" == "x" ] ; then
     echo "Please export NUCLEI_SDK_ROOT variable to correct path"
+    env_help
     exit 1
 fi
 
 if [ "x${NUCLEI_SDK_NMSIS}" == "x" ] ; then
     echo "Please export NUCLEI_SDK_NMSIS variable to correct path"
+    env_help
     exit 1
 fi
 
-if [ -d $LOGROOT ] ; then
-    mkdir -p $BACKUP
-    backupfile=$BACKUP/fpgarun_$(date -u +"%Y%m%d-%H%M%S").tar.gz
-    echo "Backup old logs to $backupfile"
-    tar -czf $backupfile $LOGROOT
-    rm -rf $LOGROOT
-fi
+function backup_logs {
+    if [ -d $LOGROOT ] ; then
+        mkdir -p $BACKUP
+        local backupfile=$BACKUP/fpgarun_$(date -u +"%Y%m%d-%H%M%S").tar.gz
+        echo "Backup old logs to $backupfile"
+        tar -czf $backupfile $LOGROOT
+        rm -rf $LOGROOT
+    fi
+}
+
+function setup_suite {
+    local cfgloc=${1:-mini}
+    local fpgaloc=${2:-latest}
+    local configpath=${SCRIPTDIR}/configs/${cfgloc}
+    local fpgapath=${SCRIPTDIR}/bitstreams/$fpgaloc
+
+    if [ ! -d $configpath ] ; then
+        echo "Can't find proper test suite $cfgloc located in $configpath"
+        echo "Please pass correct CFGSET value, such as CFGSET=mini or CFGSET=full"
+        exit 1
+    fi
+    if [ ! -d $fpgapath ] ; then
+        echo "Can't find proper test suite $fpgaloc located in $fpgapath"
+        echo "Please pass correct FPGASET value, such as FPGASET=latest or FPGASET=202206"
+        exit 1
+    fi
+    CFGLOC=$configpath
+    FPGALOC=$fpgapath
+}
 
 function run_nmsis {
     local lib=${1:-nmsis_nn}
 
     local logdir=${LOGROOT}/${lib}
 
+    if [ "x$CFGLOC" == "x" ] || [ "x$FPGALOC" == "x" ] ; then
+        echo "Can't find any fpga location and config location, please check"
+        return
+    fi
+
     mkdir -p $logdir
 
-    local runcmd="python3 $NUCLEI_SDK_ROOT/tools/scripts/nsdk_cli/nsdk_runner.py --appyaml ${NUCLEI_SDK_NMSIS}/Scripts/Configs/fpga/${lib}.yaml --logdir ${logdir} --runon fpga --config ${CONFIG}"
+    local runcmd="python3 $NUCLEI_SDK_ROOT/tools/scripts/nsdk_cli/nsdk_runner.py --appyaml ${NUCLEI_SDK_NMSIS}/Scripts/Configs/fpga/${lib}.yaml --logdir ${logdir} --runon fpga --config ${CONFIG} --cfgloc ${CFGLOC} --fpgaloc ${FPGALOC}"
     echo $runcmd
     eval $runcmd | tee $logdir/run.log
 }
+
+backup_logs
+setup_suite ${CFGSET} ${FPGASET}
 
 run_nmsis nmsis_nn
 run_nmsis nmsis_dsp
