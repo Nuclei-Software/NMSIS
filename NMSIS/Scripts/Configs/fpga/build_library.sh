3ba97c514bc6140d6495d167948aae2596fbb690
Huaqi Fang <578567190@qq.com>
2022-10-19 16:19:13 +0800

    Script: add NUCLEI_DSP variable in build_library.sh
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/Scripts/Configs/fpga/build_library.sh
+++ NMSIS/Scripts/Configs/fpga/build_library.sh
@@ -1,7 +1,8 @@
 #!/bin/env bash
 REBUILD=${REBUILD:-1}
+NUCLEI_DSP=${NUCLEI_DSP:-N3}
 
 echo "Generate DSP/NN Library"
-REBUILD=$REBUILD make gen
+REBUILD=$REBUILD NUCLEI_DSP=$NUCLEI_DSP make gen
 echo "Generate NN Reference Library"
 REBUILD=$REBUILD make gen_nnref_lib
