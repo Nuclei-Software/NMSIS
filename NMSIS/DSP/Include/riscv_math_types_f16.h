6e3bcdc7e0cf66f42ced04dbec8e42afcc7c772b
dongyongtao <dongyongtao@nucleisys.com>
2023-10-23 10:07:11 +0800

    NMSIS/DSP: protect "typedef _Float16 float16_t" by macro RISCV_FLOAT16_SUPPORTED
    Signed-off-by: dongyongtao <dongyongtao@nucleisys.com>

--- NMSIS/DSP/Include/riscv_math_types_f16.h
+++ NMSIS/DSP/Include/riscv_math_types_f16.h
@@ -43,7 +43,6 @@ If it is not available, f16 version of the kernels
 won't be built.
 
 */
-typedef _Float16 float16_t;
 
 #if (defined (__riscv_zfh))
   #undef RISCV_FLOAT16_SUPPORTED
@@ -55,12 +54,13 @@ typedef _Float16 float16_t;
 
 #if defined(RISCV_FLOAT16_SUPPORTED)
 
+typedef _Float16 float16_t;
 
 #define F16INFINITY ((float16_t)__builtin_inf())
 
 
 #define F16_MAX   ((float16_t)__FLT16_MAX__)
-#define F16_MIN   (-(_Float16)__FLT16_MAX__)
+#define F16_MIN   (-(float16_t)__FLT16_MAX__)
 
 #define F16_ABSMAX   ((float16_t)__FLT16_MAX__)
 #define F16_ABSMIN   ((float16_t)0.0f16)
