c0ba320dcdba08e83f2189f7544ca898e57a216d
Huaqi Fang <578567190@qq.com>
2025-10-27 18:57:12 +0800

    build: add NMSIS_LIB_COMPAT variable to specify toolchain builtin dsp/nn library
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/build.mk
+++ NMSIS/build.mk
@@ -3,19 +3,36 @@ NMSIS_LIB_ARCH ?= $(RISCV_ARCH)
 
 INCDIRS += $(NUCLEI_SDK_NMSIS)/Core/Include
 
+# When set to 1, will use compatiable library
+# provided in toolchain such as terapines zcc toolchain
+# present NMSIS DSP/NN compatiable library
+NMSIS_LIB_COMPAT ?=
+
 ifeq ($(filter nmsis_nn,$(NMSIS_LIB_SORTED)),nmsis_nn)
 INCDIRS += $(NUCLEI_SDK_NMSIS)/NN/Include
 LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/NN/GCC
-LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
-else ifeq ($(filter tpt_nn,$(NMSIS_LIB_SORTED)),tpt_nn)
+ifeq ($(TOOLCHAIN),terapines)
+ifeq ($(NMSIS_LIB_COMPAT),1)
 LDLIBS += -lnn
+else
+LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
+endif
+else
+LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
+endif
 endif
 
 ifeq ($(filter nmsis_dsp,$(NMSIS_LIB_SORTED)),nmsis_dsp)
 INCDIRS += $(NUCLEI_SDK_NMSIS)/DSP/Include \
 	$(NUCLEI_SDK_NMSIS)/DSP/PrivateInclude
 LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/DSP/GCC
-LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
-else ifeq ($(filter tpt_dsp,$(NMSIS_LIB_SORTED)),tpt_dsp)
+ifeq ($(TOOLCHAIN),terapines)
+ifeq ($(NMSIS_LIB_COMPAT),1)
 LDLIBS += -ldsp
+else
+LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
+endif
+else
+LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
+endif
 endif
