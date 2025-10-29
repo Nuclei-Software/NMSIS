f481c6890cc2750648c116310c4a8b9a056b44d3
Huaqi Fang <578567190@qq.com>
2025-10-29 10:05:07 +0800

    build: Fix wrong setup for nmsis library selection in DSP testing makefile
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- NMSIS/build.mk
+++ NMSIS/build.mk
@@ -9,30 +9,24 @@ INCDIRS += $(NUCLEI_SDK_NMSIS)/Core/Include
 NMSIS_LIB_COMPAT ?=
 
 ifeq ($(filter nmsis_nn,$(NMSIS_LIB_SORTED)),nmsis_nn)
-INCDIRS += $(NUCLEI_SDK_NMSIS)/NN/Include
-LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/NN/GCC
-ifeq ($(TOOLCHAIN),terapines)
-ifeq ($(NMSIS_LIB_COMPAT),1)
+ifeq ($(TOOLCHAIN)-$(NMSIS_LIB_COMPAT),terapines-1)
+# use nn library from terapines zcc toolchain when NMSIS_LIB_COMPAT=1
 LDLIBS += -lnn
 else
 LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
-endif
-else
-LDLIBS += -lnmsis_nn_$(NMSIS_LIB_ARCH)
+INCDIRS += $(NUCLEI_SDK_NMSIS)/NN/Include
+LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/NN/GCC
 endif
 endif
 
 ifeq ($(filter nmsis_dsp,$(NMSIS_LIB_SORTED)),nmsis_dsp)
-INCDIRS += $(NUCLEI_SDK_NMSIS)/DSP/Include \
-	$(NUCLEI_SDK_NMSIS)/DSP/PrivateInclude
-LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/DSP/GCC
-ifeq ($(TOOLCHAIN),terapines)
-ifeq ($(NMSIS_LIB_COMPAT),1)
+ifeq ($(TOOLCHAIN)-$(NMSIS_LIB_COMPAT),terapines-1)
+# use dsp library from terapines zcc toolchain when NMSIS_LIB_COMPAT=1
 LDLIBS += -ldsp
 else
 LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
-endif
-else
-LDLIBS += -lnmsis_dsp_$(NMSIS_LIB_ARCH)
+INCDIRS += $(NUCLEI_SDK_NMSIS)/DSP/Include \
+	$(NUCLEI_SDK_NMSIS)/DSP/PrivateInclude
+LIBDIRS += $(NUCLEI_SDK_NMSIS)/Library/DSP/GCC
 endif
 endif
