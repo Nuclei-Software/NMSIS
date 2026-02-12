1155be1d554b7f7d27dc784b6fb17a8cc78d7370
qiujiandong <qiujiandong@nucleisys.com>
2026-02-12 11:53:40 +0800

    NMSIS/DSP: More memory size is needed for RVV test in testv2
    Signed-off-by: qiujiandong <qiujiandong@nucleisys.com>

--- NMSIS/DSP/Testing/testmain.cpp
+++ NMSIS/DSP/Testing/testmain.cpp
@@ -16,7 +16,7 @@ using namespace std;
 #ifdef BENCHMARK
 #define MEMSIZE 300000
 #else
-#if defined(RISCV_MATH_NEON)
+#if defined(RISCV_MATH_VECTOR)
 #define MEMSIZE 80000000
 #else
 #define MEMSIZE 230000
